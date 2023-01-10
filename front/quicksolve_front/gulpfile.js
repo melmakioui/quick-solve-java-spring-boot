35338489968d1426b1bd1729e841e5a55cddc285
Mohamed <91630250+melmakioui@users.noreply.github.com>
2023-01-10 17:35:11 +0100

    Tareas de compilacion y minimización de bootstrap

--- front/quicksolve_front/gulpfile.js
+++ front/quicksolve_front/gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
 
 function compileSass(){
     return gulp.src('./src/main/resources/templates/scss/**/*.scss')
@@ -7,4 +8,10 @@ function compileSass(){
         .pipe(gulp.dest('./src/main/resources/templates/css'));
 }
 
-exports.default = compileSass;
+function minifyCss() {
+    return gulp.src('./src/main/resources/templates/css/**/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./src/main/resources/templates/dist'));
+}
+
+exports.default = gulp.parallel(gulp.series(compileSass,minifyCss));
