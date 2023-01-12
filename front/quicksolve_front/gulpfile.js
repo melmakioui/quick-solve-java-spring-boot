008ddd6bfc93ca1853f891fe1374be434a307d9f
AlanAndrzejSkowronek <alanskowronek1414@gmail.com>
2023-01-13 00:11:09 +0100

    Estructura basica homepage y cambios css

--- front/quicksolve_front/gulpfile.js
+++ front/quicksolve_front/gulpfile.js
@@ -3,15 +3,15 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 
 function compileSass(){
-    return gulp.src('./src/main/resources/templates/scss/**/*.scss')
+    return gulp.src('./src/main/resources/static/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./src/main/resources/templates/css'));
+        .pipe(gulp.dest('./src/main/resources/static/css'));
 }
 
 function minifyCss() {
-    return gulp.src('./src/main/resources/templates/css/**/*.css')
+    return gulp.src('./src/main/resources/static/css/**/*.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./src/main/resources/templates/dist'));
+        .pipe(gulp.dest('./src/main/resources/static/dist'));
 }
 
-exports.build = gulp.parallel(gulp.series(compileSass,minifyCss));
+exports.default = gulp.parallel(gulp.series(compileSass,minifyCss));
