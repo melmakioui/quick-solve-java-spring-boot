102c6014fe3a6c03b0b2bb8df9415d182f03a313
Alan Skowronek <alanskowronek1414@gmail.com>
2023-01-20 09:53:37 +0100

    Inicio de login.

--- front/quicksolve_front/src/main/java/com/quicksolve/proyecto/controller/IncidenceController.java
+++ front/quicksolve_front/src/main/java/com/quicksolve/proyecto/controller/IncidenceController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@SessionAttributes({"user"})
 public class IncidenceController {
 
     private final IncidenceService incidenceService;
--- front/quicksolve_front/src/main/java/com/quicksolve/proyecto/controller/LoginController.java
+++ front/quicksolve_front/src/main/java/com/quicksolve/proyecto/controller/LoginController.java
@@ -3,16 +3,26 @@ package com.quicksolve.proyecto.controller;
 import com.quicksolve.proyecto.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@SessionAttributes({"user"})
 public class LoginController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/login")
-    public String renderLogin(){
+    public String renderLogin(Model model){
+        model.addAttribute("user",null);
         return "view/login";
     }
+
+    @PostMapping("/login")
+    public String login(){
+        
+        return "view/login";
+    }
+
 }
