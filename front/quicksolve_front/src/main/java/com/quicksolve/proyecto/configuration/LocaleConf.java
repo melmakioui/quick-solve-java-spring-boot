7e07ea2fbb0ce511945ab2388dcd120f0e625c1e
Alan Skowronek <alanskowronek1414@gmail.com>
2023-01-24 08:56:03 +0100

    Config de los idiomas.

---
+++ front/quicksolve_front/src/main/java/com/quicksolve/proyecto/configuration/LocaleConf.java
@@ -0,0 +1,34 @@
+package com.quicksolve.proyecto.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class LocaleConf implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("localeData");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry){
+        interceptorRegistry.addInterceptor(localeChangeInterceptor());
+    }
+}
