45ec12621aae427a12853e121b71cdcc76cafdf7
Alan Skowronek <alanskowronek1414@gmail.com>
2023-01-31 01:55:33 +0100

    Cambio de validacion del registro por cliente hecho.

--- front/quicksolve_front/src/main/resources/static/js/validationRegister.js
+++ front/quicksolve_front/src/main/resources/static/js/validationRegister.js
@@ -1,60 +1,19 @@
 $(document).ready(function () {
-    const regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/;
-
-    $("#confirmpassword").on('keyup', function(){
-        const password = $("#password").val();
-        const confirmPassword = $("#confirmpassword").val();
-        if (password !== confirmPassword) {
-            $("#passwordMatchError").html("Las contraseñas no coinciden.");
-            $("#password").addClass("is-invalid");
-            $("#confirmpassword").addClass("is-invalid");
-        } else {
-            $("#passwordMatchError").html("");
-            $("#password").removeClass("is-invalid");
-            $("#confirmpassword").removeClass("is-invalid");
-        }
-        activateButtonForm();
+    $("#password").on('input', function(){
+        $("#confirmpassword").attr("pattern", "^" + $("#password").val() + "$")
     });
 
-    $("#name").on('keyup', function(){
-        const name = $("#name").val();
-        console.log(regex.test(name))
-        if (name === "" && !regex.test(name)){
-            $("#nameBlankError").html("El nombre es obligatorio.");
-            $("#name").addClass("is-invalid");
-        } else {
-            $("#nameBlankError").html("");
-            $("#name").removeClass("is-invalid");
-        }
-        activateButtonForm();
-    });
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation');
+    Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
 
-    $("#firstsurname").on('keyup', function(){
-        const firstsurname = $("#firstsurname").val();
-
-        if(firstsurname === "" && !regex.test(firstsurname)){
-            $("#firstSurnameBlankError").html("El primer apellido es obligatorio.");
-            $("#firstsurname").addClass("is-invalid");
-        } else {
-            $("#firstSurnameBlankError").html("");
-            $("#firstsurname").removeClass("is-invalid");
-        }
-        activateButtonForm();
+            form.classList.add('was-validated')
+        }, false)
     });
 });
-
-function activateButtonForm(){
-    const name = $("#name").val();
-    const firstSurname = $("#firstsurname").val();
-    const password = $("#password").val();
-    const confirmpassword = $("#confirmpassword").val();
-
-    if (name !== "" &&
-        firstSurname !== "" &&
-        password !== "" &&
-        password === confirmpassword){
-        $("#submit").removeClass("disabled");
-    } else {
-        $("#submit").addClass("disabled");
-    }
-}
