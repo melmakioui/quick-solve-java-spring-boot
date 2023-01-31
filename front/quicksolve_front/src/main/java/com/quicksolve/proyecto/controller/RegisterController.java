34f13a767f007cf7fb638bf943762dd477f30dbe
Alan Skowronek <alanskowronek1414@gmail.com>
2023-01-31 02:15:14 +0100

    Validacion por parte del servidor del username y email repetidos.

--- front/quicksolve_front/src/main/java/com/quicksolve/proyecto/controller/RegisterController.java
+++ front/quicksolve_front/src/main/java/com/quicksolve/proyecto/controller/RegisterController.java
@@ -27,27 +27,30 @@ public class RegisterController {
     public String renderRegister(Model model){
         User user = new User();
         model.addAttribute("user", user);
+        model.addAttribute("alreadyExistsEmail", "false");
+        model.addAttribute("alreadyExistsUsername", "false");
         return "view/registro";
     }
 
     @PostMapping("/registro")
     public String registerUser(
-            @Valid FullUserDTO user,
-            BindingResult bindingResult,
+            FullUserDTO user,
             @RequestParam("name") String name,
             @RequestParam("firstSurname") String firstSurname,
             @RequestParam(value = "secondSurname", required = false) String secondSurname,
             Model model
     ){
 
-        if(bindingResult.hasErrors()){
-            model.addAttribute("user", user);
-            if (userService.existsWithEmail(user.getEmail())){
-                model.addAttribute("alreadyExistsEmail", "Este email ya existe.");
-            }
-            if (userService.existsWithUsername(user.getUsername())){
-                model.addAttribute("alreadyExistsUsername", "Este nombre ya existe.");
-            }
+        model.addAttribute("user", user);
+
+        if (userService.existsWithEmail(user.getEmail())){
+            model.addAttribute("alreadyExistsEmail", "true");
+        }
+        if (userService.existsWithUsername(user.getUsername())){
+            model.addAttribute("alreadyExistsUsername", "true");
+        }
+
+        if (userService.existsWithEmail(user.getEmail()) || userService.existsWithUsername(user.getUsername())){
             return "view/registro";
         }
 
