afeabf9d09bcbc95a4ee7567f95bf90eb43ee638
Mohamed <91630250+melmakioui@users.noreply.github.com>
2023-02-12 13:04:04 +0100

    Servicio historial y traduccion incidence.html

---
+++ front/quicksolve_front/src/main/java/com/quicksolve/proyecto/service/implementation/HistoryServiceImpl.java
@@ -0,0 +1,47 @@
+package com.quicksolve.proyecto.service.implementation;
+
+import com.quicksolve.proyecto.entity.History;
+import com.quicksolve.proyecto.entity.Incidence;
+import com.quicksolve.proyecto.entity.UserIncidence;
+import com.quicksolve.proyecto.repository.HistoryRepository;
+import com.quicksolve.proyecto.repository.UserIncidenceRepository;
+import com.quicksolve.proyecto.service.HistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class HistoryServiceImpl implements HistoryService {
+
+
+    @Autowired
+    private HistoryRepository historyRepository;
+
+    @Autowired
+    private UserIncidenceRepository userIncidenceRepository;
+
+    @Override
+    public void saveHistory(long incidenceId) {
+
+        UserIncidence ui = userIncidenceRepository.findByIncidenceId(incidenceId);
+        Incidence incidence = ui.getIncidence();
+
+        History history = new History();
+
+        history.setTechId(ui.getTech() == null ? 0L : ui.getTech().getId());
+        history.setDepartmentId(incidence.getDepartment() == null ? 0L : incidence.getDepartment().getId());
+        history.setSpaceId(incidence.getSpace() == null ? 0L : incidence.getSpace().getId());
+        history.setDateEnd(incidence.getDateEnd() == null ? null : incidence.getDateEnd());
+        history.setIncidenceId(incidence.getId());
+        history.setStateId(incidence.getIncidenceState().getId());
+        history.setTitle(incidence.getTitle());
+        history.setDescription(incidence.getDescription());
+        history.setDateStart(incidence.getDateStart());
+        history.setChangeDate(LocalDateTime.now());
+
+        historyRepository.save(history);
+    }
+
+}
+
