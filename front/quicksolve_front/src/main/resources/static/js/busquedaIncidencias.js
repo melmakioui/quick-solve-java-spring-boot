78a9c9ab9de731fc5cb409c33bcc218b9afbfddb
Alan Skowronek <alanskowronek1414@gmail.com>
2023-02-16 09:42:41 +0100

    Traducida pagina incidences tech.

--- front/quicksolve_front/src/main/resources/static/js/busquedaIncidencias.js
+++ front/quicksolve_front/src/main/resources/static/js/busquedaIncidencias.js
@@ -8,23 +8,35 @@ $(document).ready(function (){
                 data: evt.currentTarget.value,
                 success: function (data) {
                     $("#resultados-incidencias").html("")
-                    data.forEach(i => {
-                        $("#resultados-incidencias").append(`
-                            <div class="card card-animation card mb-4 mx-2 incidence" style="width: 18rem;" onclick="window.location.href='/incidencia/${i.id}'">
-                                <div class="card-body">
-                                    <h5 class="card-title text-truncate">${i.title}</h5>
-                                    <p class="card-text text-truncate mb-1">${i.description}</p>
-                                    <div class="d-flex justify-content-between">
-                                        <small>${i.daysAgo}</small>
+                    if (data.length !== 0){
+                        data.forEach(i => {
+                            $("#resultados-incidencias").append(`
+                                <div class="card card-animation card mb-4 mx-2 incidence" style="width: 18rem;" onclick="window.location.href='/incidencia/${i.id}'">
+                                    <div class="card-body">
+                                        <h5 class="card-title text-truncate">${i.title}</h5>
+                                        <p class="card-text text-truncate mb-1">${i.description}</p>
+                                        <div class="d-flex justify-content-between">
+                                            <small>${i.dateStart}</small>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        `)
-                    })
+                            `);
+                        });
+                    } else {
+                        notFoundResults();
+                    }
                 }
-            })
+            });
         } else {
-            $("#resultados-incidencias").html("")
+            notFoundResults();
         }
-    })
-})
+    });
+});
+
+function notFoundResults(){
+    $("#resultados-incidencias").html(`
+       <div class="d-flex justify-content-center text-center flex-column">
+            <img src="/img/person-dealing-error.svg" class="img-fluid mt-2" style="max-width: 385px;">
+       </div>
+    `);
+}
