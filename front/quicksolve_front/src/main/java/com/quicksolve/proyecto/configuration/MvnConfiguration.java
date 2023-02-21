cc646f4dd20ac9a040a9bc042e5f4bd447baf172
Mohamed <91630250+melmakioui@users.noreply.github.com>
2023-02-21 19:03:44 +0100

    Fix fragments

--- front/quicksolve_front/src/main/java/com/quicksolve/proyecto/configuration/MvnConfiguration.java
+++ front/quicksolve_front/src/main/java/com/quicksolve/proyecto/configuration/MvnConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.*;
 
