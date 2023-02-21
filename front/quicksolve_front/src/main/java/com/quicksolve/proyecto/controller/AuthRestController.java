dfbe0de8b83d256c60da486dcf8c6073dd94d6de
Alan Skowronek <alanskowronek1414@gmail.com>
2023-02-21 08:46:18 +0100

    Borrado tokenrest y refactor de authrest.

--- front/quicksolve_front/src/main/java/com/quicksolve/proyecto/controller/AuthRestController.java
+++ front/quicksolve_front/src/main/java/com/quicksolve/proyecto/controller/AuthRestController.java
@@ -2,6 +2,7 @@ package com.quicksolve.proyecto.controller;
 
 import com.quicksolve.proyecto.configuration.PasswordEncoderConf;
 import com.quicksolve.proyecto.dto.FullUserDTO;
+import com.quicksolve.proyecto.entity.type.UserType;
 import com.quicksolve.proyecto.service.TokenService;
 import com.quicksolve.proyecto.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,20 +30,33 @@ public class AuthRestController {
 
         if(userService.existsWithEmail(email)) {
             FullUserDTO userDTO = userService.getUserBy(email);
-            if (passwordEncoder.encoder().matches(password, userDTO.getPassword())) {
+            return crearTokenPasswordValid(email, password, userDTO);
+        }
+        return new ResponseEntity<>("User not authorized", HttpStatus.UNAUTHORIZED);
+    }
 
-                String rol = "";
-                if (userDTO.getType() != null) {
-                    rol = userDTO.getType().name();
-                }
+    @PostMapping(value = "/loginTokenGeneratePrivate")
+    public @ResponseBody ResponseEntity<?> validacioLoginPrivada(@RequestBody String jsonData) throws JSONException {
+        JSONObject json = new JSONObject(jsonData);
+        String email = json.getString("email");
+        String password = json.getString("password");
 
-                return new ResponseEntity<>(tokenService.createToken(email, rol), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("User not authorized", HttpStatus.UNAUTHORIZED);
-            }
+        if(userService.existsWithEmail(email)) {
+            FullUserDTO userDTO = userService.getUserBy(email);
+            if (userDTO.getType() == UserType.ADMIN) return crearTokenPasswordValid(email, password, userDTO);
+            return new ResponseEntity<>("User is not administrator", HttpStatus.UNAUTHORIZED);
         } else {
             return new ResponseEntity<>("User not authorized", HttpStatus.UNAUTHORIZED);
         }
+    }
+
+    private ResponseEntity<?> crearTokenPasswordValid(String email, String password, FullUserDTO userDTO) {
+        if (passwordEncoder.encoder().matches(password, userDTO.getPassword())) {
+            String rol = "";
+            if (userDTO.getType() != null) rol = userDTO.getType().name();
+            return new ResponseEntity<>(tokenService.createToken(email, rol), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("User not authorized", HttpStatus.UNAUTHORIZED);
     }
 
 }
