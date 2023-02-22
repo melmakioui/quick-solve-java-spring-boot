d48b7d7b584488dca1d51b597c1c5839deac7171
Mohamed <91630250+melmakioui@users.noreply.github.com>
2023-02-22 09:38:25 +0100

    Pequeñas correcciones js, html

--- front/quicksolve_front/src/main/resources/static/js/filterIncidences.js
+++ front/quicksolve_front/src/main/resources/static/js/filterIncidences.js
@@ -1,12 +1,15 @@
 $(document).ready(function (){
 
-    const options = {
+    $( "#datepicker" ).datepicker({
         dateFormat: "yy-mm-dd",
-        maxDate: new Date()
-    }
-
-    $( "#datepicker" ).datepicker(options);
-    $( "#datepickerResponsive" ).datepicker(options);
+        maxDate: new Date(),
+        culture: "es",
+    },
+       );
+    $( "#datepickerResponsive" ).datepicker({
+        dateFormat: "yy-mm-dd",
+        maxDate: new Date(),
+    });
 
     const btnCanvas = $('#offcanvasBtn')
     const btnCanvasContainer= $("#offCanvasBtnCont")
@@ -31,21 +34,60 @@ $(document).ready(function (){
     }
 
 
-    const accordions = $(".incidences");
-    const message = $("#emptyMessage")
-    const messageTrue = $(message).clone()
-
-    $.each(accordions,function (index,accordion){
-        if (accordion.children.length === 0) {
-            $(message).remove()
-            $(messageTrue).removeClass("d-none")
-            $(accordion).append($(messageTrue))
-        }
-    })
-
-
     var firstAccordionShow = $('.accordion-collapse')[0]
     $(firstAccordionShow).addClass('show')
     var firstAccordion = $('.accordion-button')[0]
     $(firstAccordion).removeClass('collapsed')
+
+    //Idiomas para el datepicker
+    $.datepicker.regional[ "es" ] = {
+        closeText: "Cerrar",
+        prevText: "Ant",
+        nextText: "Sig",
+        currentText: "Hoy",
+        monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+            "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre" ],
+        monthNamesShort: [ "ene", "feb", "mar", "abr", "may", "jun",
+            "jul", "ago", "sep", "oct", "nov", "dic" ],
+        dayNames: [ "domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado" ],
+        dayNamesShort: [ "dom", "lun", "mar", "mié", "jue", "vie", "sáb" ],
+        dayNamesMin: [ "D", "L", "M", "X", "J", "V", "S" ],
+        weekHeader: "Sm",
+        dateFormat: "dd/mm/yy",
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ""
+    };
+
+    $.datepicker.regional["en-GB"] = {
+        closeText: "Done",
+        prevText: "Prev",
+        nextText: "Next",
+        currentText: "Today",
+        monthNames: [ "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December" ],
+        monthNamesShort: [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ],
+        dayNames: [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ],
+        dayNamesShort: [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ],
+        dayNamesMin: [ "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa" ],
+        weekHeader: "Wk",
+        dateFormat: "dd/mm/yy",
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ""
+    }
+    $.datepicker.setDefaults($.datepicker.regional["es"])
+
+    var language = document.cookie.split("=")
+    language = language[language.length -1]
+
+    if (language === "en"){
+        $.datepicker.setDefaults($.datepicker.regional["en-GB"])
+    }else {
+        $.datepicker.setDefaults($.datepicker.regional["es"])
+    }
+
 })
