b9e3cfe1d57edac70dd1419d924de84f12f7a113
Mohamed <91630250+melmakioui@users.noreply.github.com>
2023-02-25 12:16:54 +0100

    Handle idioma por si no existe

--- front/quicksolve_front/src/main/java/com/quicksolve/proyecto/service/implementation/LanguageServiceImpl.java
+++ front/quicksolve_front/src/main/java/com/quicksolve/proyecto/service/implementation/LanguageServiceImpl.java
@@ -7,13 +7,13 @@ import com.quicksolve.proyecto.repository.WebPageLanguageRepository;
 import com.quicksolve.proyecto.repository.WebPageRepository;
 import com.quicksolve.proyecto.service.LanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Locale;
 @Service
 public class LanguageServiceImpl implements LanguageService {
+
+    private final String DEFAULT_LANGUAGE = "es";
 
     @Autowired
     private LanguageRepository languageRepository;
@@ -24,6 +24,10 @@ public class LanguageServiceImpl implements LanguageService {
     public List<LanguageDTO> getLanguage(String language) {
 
         Long languageId = languageRepository.findByName(language).getId();
+
+        if (languageId == null) {
+            languageId = languageRepository.findByName(DEFAULT_LANGUAGE).getId();
+        }
 
         return webPageLanguageRepository.findAllByLanguageId(languageId)
                 .stream().map(webPageLanguage -> {
