0fb23bcddb5b61205446f12be205eb5581031816
Mohamed <91630250+melmakioui@users.noreply.github.com>
2023-02-28 18:26:05 +0100

    Concat y minify, gulp

--- front/quicksolve_front/gulpfile.js
+++ front/quicksolve_front/gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const concat = require('gulp-concat');
 
 function compileSass(){
     return gulp.src('./src/main/resources/static/scss/**/*.scss')
@@ -11,6 +13,18 @@ function compileSass(){
 function minifyCss() {
     return gulp.src('./src/main/resources/static/css/**/*.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./src/main/resources/static/dist'));
+}
+
+function minifyJs() {
+    return gulp.src('./src/main/resources/static/js/**/*.js')
+        .pipe(minify())
+        .pipe(gulp.dest('./src/main/resources/static/dist'));
+}
+
+function concatCss(){
+    return gulp.src('./src/main/resources/static/css/**/*.css')
+        .pipe(concat('styles.css'))
         .pipe(gulp.dest('./src/main/resources/static/dist'));
 }
 
